5d6ca7c32bd58943382a82de59c81fa47a0d220a
Lewis Wale <lew.wale@gmail.com>
2020-06-14 17:16:54 +0100

    added poker page and visual display of hand

--- webserver/pages/poker.js
+++ webserver/pages/poker.js
@@ -1,16 +1,35 @@
 import Link from 'next/link'
 import Head from 'next/head'
 import {useState, Component} from 'react'
+import cardsStyle from '../styles/cards.module.css'
 
 class Poker extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            card1: "",
-            card2: ""
+            card1: "14S",
+            card2: "14H",
+            card1img: "AS",
+            card2img: "AH"
         };
 
         this.getHand = this.getHand.bind(this);
+        this.getImageName = this.getImageName.bind(this);
+    }
+
+    getImageName(value, suit) {
+        switch(value) {
+            case 11:
+                return 'J' + suit;
+            case 12:
+                return 'Q' + suit;
+            case 13:
+                return 'K' + suit;
+            case 14:
+                return 'A' + suit;
+            default:
+                return value + suit;
+        }
     }
 
     async getHand(event) {
@@ -19,7 +38,19 @@ class Poker extends Component {
         })
         .then((res => res.json()))
         .then((responseJson) => {
-            this.setState({card1: responseJson[0]['value'] + responseJson[0]['suit'], card2: responseJson[1]['value'] + responseJson[1]['suit']})
+            var value1 = responseJson[0]['value'];
+            var suit1 = responseJson[0]['suit'];
+            var value2 = responseJson[1]['value'];
+            var suit2 = responseJson[1]['suit'];
+            var img1 = this.getImageName(value1, suit1);
+            var img2 = this.getImageName(value2, suit2);
+
+            this.setState({
+                card1: value1 + suit1,
+                card2: value2 + suit2,
+                card1img: img1,
+                card2img: img2
+            });
         });
 
     }
@@ -33,7 +64,7 @@ class Poker extends Component {
 
                 <body>
                     <div>
-                        {this.state.card1} {this.state.card2}
+                        <img className={cardsStyle.card} src={`/cards/${this.state.card1img}.png`}/> <img className={cardsStyle.card} src={`/cards/${this.state.card2img}.png`}/>
                     </div>
                     <div>
                         <button onClick={this.getHand}>
